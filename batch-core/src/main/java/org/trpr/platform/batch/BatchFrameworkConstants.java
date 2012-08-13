812ef13c0fee16f9ebc6547194756d59ad1f6a46
Regunath B <regunathb@gmail.com>
2012-08-13 11:11:45 +0530

    First commits for Trooper batch core

---
+++ batch-core/src/main/java/org/trpr/platform/batch/BatchFrameworkConstants.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2012-2015, the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.trpr.platform.batch;
+
+/**
+ * 
+ * The <code>BatchFrameworkConstants</code> class is a placeholder for all batch framework
+ * constants.
+ * 
+ * @author Regunath B
+ * 
+ */
+public abstract class BatchFrameworkConstants {
+
+	/**
+	 * Constants for the conventions on config file names
+	 */
+	public static final String COMMON_BATCH_CONFIG = "common-batch-config.xml";
+
+	public static final String SPRING_BATCH_CONFIG = "spring-batch-config.xml";
+	
+}
