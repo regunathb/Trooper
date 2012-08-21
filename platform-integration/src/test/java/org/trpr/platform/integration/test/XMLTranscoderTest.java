e1c3d4c085bb01ffbcaefe5d0007d05e015c63e8
Regunath B <regunathb@gmail.com>
2012-08-21 18:24:35 +0530

    Changes to accommodate XJC serialization aspects

--- platform-integration/src/test/java/org/trpr/platform/integration/test/XMLTranscoderTest.java
+++ platform-integration/src/test/java/org/trpr/platform/integration/test/XMLTranscoderTest.java
@@ -1,5 +1,6 @@
 package org.trpr.platform.integration.test;
 
+
 /**
  * Test class for org.trpr.platform.integration.impl.xml.XMLTranscoderImpl 
  * 
@@ -11,13 +12,15 @@ public class XMLTranscoderTest {
 	public static void main(String[] args) {
 		org.trpr.platform.model.event.PlatformEvent event = new org.trpr.platform.model.event.PlatformEvent();
 		event.setEventMessage("Hi there");
-		event.setCreatedDate(java.util.Calendar.getInstance());		
-		String xml = new org.trpr.platform.integration.impl.xml.XMLTranscoderImpl().marshal(event);
+		event.setCreatedDate(java.util.Calendar.getInstance());	
+		org.trpr.platform.model.event.PlatformEventXML eventXML = new org.trpr.platform.model.event.PlatformEventXML();
+		eventXML.setPlatformEvent(event);
+		String xml = new org.trpr.platform.integration.impl.xml.XMLTranscoderImpl().marshal(eventXML);
 		// edit the generated XML to introduce non-existent tags in the schema to test if unmarshaling can ignore extra elements in XML 
 		xml = xml.replace("</ns2:createdDate>", "</ns2:createdDate>\n<ns2:test>test text</ns2:test>");
 		System.out.println(xml);
-		System.out.println(((org.trpr.platform.model.event.PlatformEvent)
-				new org.trpr.platform.integration.impl.xml.XMLTranscoderImpl().unmarshal(xml,org.trpr.platform.model.event.PlatformEvent.class)).getEventMessage());
+		System.out.println(((org.trpr.platform.model.event.PlatformEventXML)
+				new org.trpr.platform.integration.impl.xml.XMLTranscoderImpl().unmarshal(xml,org.trpr.platform.model.event.PlatformEventXML.class)).getPlatformEvent().getEventMessage());
 	}
 	
 }
