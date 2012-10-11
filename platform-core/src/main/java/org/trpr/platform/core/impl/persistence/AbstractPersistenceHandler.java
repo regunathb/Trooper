02146e09a3c58d796feeed8aae8f69cda5675147
Regunath B <regunathb@gmail.com>
2012-10-11 14:39:25 +0530

    Defined abstract implementation of PersistenceHandler

---
+++ platform-core/src/main/java/org/trpr/platform/core/impl/persistence/AbstractPersistenceHandler.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2012-2015, the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.trpr.platform.core.impl.persistence;
+
+import org.trpr.platform.core.PlatformConstants;
+import org.trpr.platform.core.spi.management.jmx.InstanceAwareMBean;
+import org.trpr.platform.core.spi.persistence.PersistenceHandler;
+
+/**
+ * <code>AbstractPersistenceHandler<code> is a convenience implementation of {@link PersistenceHandler} that has behavior common to all persistence handlers such
+ * as JMX object naming behavior 
+ *
+ * @author Regunath B
+ * @version 1.0, 11/10/2012
+ */
+
+public abstract class AbstractPersistenceHandler implements PersistenceHandler, InstanceAwareMBean {
+	
+	/**
+	 * No arg constructor.
+	 */
+	public AbstractPersistenceHandler(){
+	}
+
+	/**
+	 * Interface method implementation. Returns a bean name suffix that comprises of: <escaped Trooper app name>,"handler=<beanKey>"
+	 * @see InstanceAwareMBean#getMBeanNameSuffix(Object, String)
+	 */
+	public String getMBeanNameSuffix(Object managedBean, String beanKey) {
+		return String.format(escapeForObjectName(System.getProperty(PlatformConstants.TRPR_APP_NAME)) + ",handler=%s", beanKey);
+	}
+	
+	/**
+	 * Helper method to escape characters for JMX object naming
+	 */
+	private String escapeForObjectName(String value) {
+		value = value.replaceAll(" ", "_");
+		value = value.replaceAll(",", ";");
+		value = value.replaceAll("=", "~");
+		value = value.replaceAll(":", "@");
+		value = value.replaceAll(",", ";");
+		value = value.replaceAll("=", "~");
+		return value;
+	}	
+	
+
+}
